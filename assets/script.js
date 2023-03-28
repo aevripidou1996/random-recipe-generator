b84f30b7b95a9c58969902be025b1badf553fefe
Andrew Evripidou <aevripidou1996@gmail.com>
2023-03-28 19:14:34 +1100

    added api

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const getMeal = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        createMeal(data.meals[0]);
+      });
+  };
